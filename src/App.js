8ba37e81c95dc43b5ea12ea82a88d1667381c614
MrPhyaeSoneThwim <mr.phyaesonethwim1998@gmail.com>
2022-07-09 21:16:09 +0630

    soved test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Hello, Welcome from the DEV Zone.</p>
+				<p>Let's learn react from www.reactjs.com</p>
 			</header>
 		</div>
 	);
