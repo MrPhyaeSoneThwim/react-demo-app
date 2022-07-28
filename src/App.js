1c778210ab4b8c6648c2bc22656da8bb88da3da8
MrPhyaeSoneThwim <mr.phyaesonethwim1998@gmail.com>
2022-07-28 14:45:25 +0630

    updated testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, Welcome from NexStack</p>
+        <p>Hello, Welcome from NexStack and learn react</p>
       </header>
     </div>
   );
