9759de4c01c6d33f83ee473f391bcaf233314916
MrPhyaeSoneThwim <mr.phyaesonethwim1998@gmail.com>
2022-07-28 14:38:37 +0630

    updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello, Welcome from the DEV Zone.</p>
-				<p>Let's learn react from www.reactjs.com</p>
-				<p>Let's test CICD another time.</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, Welcome from NexStack</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
